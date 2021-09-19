23c8073da9d0554ddc1389b418c8ae62efe4c937
mmusab <muhammadmusab92@gmail.com>
2021-09-19 22:44:48 +0500

    changing to public ip correction

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -39,7 +39,7 @@ export class LoginScreenComponent{
     // this.resetErrors();
     if(this.userEmail == ""){
     }
-    this.httpClient.get('http://82.69.10,205:5002/login/' + this.userEmail + '/' + this.userPassword).toPromise().then(message => {
+    this.httpClient.get('http://82.69.10.205:5002/login/' + this.userEmail + '/' + this.userPassword).toPromise().then(message => {
       this.errorMessage = (message as any)[0]['message'];
       if(this.errorMessage == "Welcome"){
         UserLogin.userEmail = this.userEmail;
@@ -65,8 +65,8 @@ export class LoginScreenComponent{
   onregister(){
     // this.resetErrors();
     var url = "";
-    if(this.userEmail){url = 'http://82.69.10,205:5002/login/' + this.userEmail + '/none'}
-    else{url = 'http://82.69.10,205:5002/login/none/none'}
+    if(this.userEmail){url = 'http://82.69.10.205:5002/login/' + this.userEmail + '/none'}
+    else{url = 'http://82.69.10.205:5002/login/none/none'}
     this.httpClient.get(url).toPromise().then(message => {
       this.errorMessage = (message as any)[0]['message'];
       if(this.errorMessage == "Email address not found" || this.userEmail == ""){
@@ -85,8 +85,8 @@ export class LoginScreenComponent{
     else{
       this.forgotPasswordError = ""
       var url = "";
-      if(this.userEmail){url = 'http://82.69.10,205:5002/login/' + this.userEmail + '/none'}
-      else{url = 'http://82.69.10,205:5002/login/none/none'}
+      if(this.userEmail){url = 'http://82.69.10.205:5002/login/' + this.userEmail + '/none'}
+      else{url = 'http://82.69.10.205:5002/login/none/none'}
       this.httpClient.get(url).toPromise().then(message => {
         this.errorMessage = (message as any)[0]['message'];
         if(this.errorMessage == "Email address not found" || this.userEmail == ""){
